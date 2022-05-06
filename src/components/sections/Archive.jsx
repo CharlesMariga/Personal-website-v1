8cc2091729de53efafc5acb73e100fdc414de8e4
charlesmariga <charlesmariga37@gmail.com>
2022-05-06 22:50:16 +0300

    Edited archive page

--- src/components/sections/Archive.jsx
+++ src/components/sections/Archive.jsx
@@ -1,132 +1,65 @@
-import React, { useState, useEffect } from "react";
-import { Link } from "gatsby";
-
+import React from "react";
 import styled from "styled-components";
 import { motion } from "framer-motion";
 
 import { SectionHeading, TertiaryHeading } from "../global";
 import Icon from "../Icon";
-import { usePreferredReducedMotion } from "../../hooks";
+import { Link } from "gatsby";
 
 const Archive = ({ data }) => {
-  const prefersReducedMotion = usePreferredReducedMotion();
-  const [isMounted, setIsMounted] = useState(false);
-
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
-
   return (
     <Wrapper>
-      {isMounted && (
-        <ArchiveSection>
-          {/* Header */}
-          {prefersReducedMotion ? (
-            <>
-              <SectionHeading title="Archive" />
-              <FlexContainer>
-                <TertiaryHeading title="My long list of projects" className="tertiary-heading" />
-                <Link to="/" className="back-link">
-                  <span>&larr;</span>
-                  <span>Back to home</span>
-                </Link>
-              </FlexContainer>
-            </>
-          ) : (
-            <motion.div
-              whileInView={{ opacity: [0, 1], y: [40, 0] }}
-              transition={{
-                duration: 0.5,
-                ease: [0.645, 0.045, 0.355, 1],
-              }}
-              viewport={{ once: true }}
-            >
-              <SectionHeading title="Archive" />
-              <FlexContainer>
-                <TertiaryHeading title="My long list of projects" className="tertiary-heading" />
-                <Link to="/" className="back-link">
-                  <span>&larr;</span>
-                  <span>Back to home</span>
-                </Link>
-              </FlexContainer>
-            </motion.div>
-          )}
+      <ArchiveSection>
+        <motion.div
+          whileInView={{ opacity: [0, 1], y: [40, 0] }}
+          transition={{
+            delay: 0.5,
+            duration: 0.5,
+            ease: [0.645, 0.045, 0.355, 1],
+          }}
+          viewport={{ once: true }}
+        >
+          <SectionHeading title="Archive" />
+          <FlexContainer>
+            <TertiaryHeading title="My long list of projects" className="tertiary-heading" />
+            <Link to="/" className="back-link">
+              <span>&larr;</span>
+              <span>Back to home</span>
+            </Link>
+          </FlexContainer>
+        </motion.div>
+        <motion.div
+          whileInView={{ opacity: [0, 1], y: [40, 0] }}
+          transition={{
+            delay: 0.8,
+            duration: 0.5,
+            ease: [0.645, 0.045, 0.355, 1],
+          }}
+          viewport={{ once: true }}
+        >
+          <Table>
+            <TableHeader>
+              <motion.tr
+                whileInView={{ opacity: [0, 1], y: [40, 0] }}
+                transition={{
+                  delay: 0.5,
+                  duration: 0.5,
+                  ease: [0.645, 0.045, 0.355, 1],
+                }}
+                viewport={{ once: true }}
+              >
+                <TableHead>Year</TableHead>
+                <TableHead>Title</TableHead>
+                <TableHead className="hide-on-mobile">Made while at</TableHead>
+                <TableHead className="hide-on-mobile">Build with</TableHead>
+                <TableHead>Link</TableHead>
+              </motion.tr>
+            </TableHeader>
+            <TableBody>
+              {data.map((project, index) => {
+                const { date, title, company, tech, external, github, ios, playstore } = project.frontmatter;
 
-          {/* Body / Table */}
-          {prefersReducedMotion ? (
-            <Table>
-              <TableHeader>
-                <tr>
-                  <TableHead>Year</TableHead>
-                  <TableHead>Title</TableHead>
-                  <TableHead className="hide-on-mobile">Made while at</TableHead>
-                  <TableHead className="hide-on-mobile">Build with</TableHead>
-                  <TableHead>Link</TableHead>
-                </tr>
-              </TableHeader>
-              <TableBody>
-                {data.map((project, index) => {
-                  const { date, title, company, tech, external, github, ios, playstore } = project.frontmatter;
-                  return (
-                    <tr key={index}>
-                      <TableData className="year">{new Date(date).getFullYear()}</TableData>
-                      <TableData className="title">{title}</TableData>
-                      <TableData className="made-at hide-on-mobile">
-                        {company ? <span>{company}</span> : <span>--</span>}
-                      </TableData>
-                      <TableData className="build-with hide-on-mobile">
-                        {tech.length &&
-                          tech.map((item, i) => (
-                            <span key={i}>
-                              {" "}
-                              {item} {i !== tech.length - 1 && <span>&middot;</span>}
-                            </span>
-                          ))}
-                      </TableData>
-                      <TableData className="link">
-                        {external && (
-                          <a href={external} aria-label="External link" target="_blank" rel="noreferrer">
-                            <Icon name="External" />
-                          </a>
-                        )}
-                        {github && (
-                          <a href={github} aria-label="GitHub link" target="_blank" rel="noreferrer">
-                            <Icon name="GitHub" />
-                          </a>
-                        )}
-                        {ios && (
-                          <a href={ios} aria-label="GitHub link" target="_blank" rel="noreferrer">
-                            <Icon name="AppStore" />
-                          </a>
-                        )}
-                        {playstore && (
-                          <a
-                            href={playstore}
-                            aria-label="Playstore link"
-                            target="_blank"
-                            rel="noreferrer"
-                            className="playstore-icon"
-                          >
-                            <Icon name="PlayStore" />
-                          </a>
-                        )}
-                      </TableData>
-                    </tr>
-                  );
-                })}
-              </TableBody>
-            </Table>
-          ) : (
-            <motion.div
-              whileInView={{ opacity: [0, 1], y: [40, 0] }}
-              transition={{
-                delay: 0.8,
-                ease: [0.645, 0.045, 0.355, 1],
-              }}
-              viewport={{ once: true }}
-            >
-              <Table>
-                <TableHeader>
+                return (
                   <motion.tr
                     whileInView={{ opacity: [0, 1], y: [40, 0] }}
                     transition={{
@@ -135,80 +68,57 @@ const Archive = ({ data }) => {
                       ease: [0.645, 0.045, 0.355, 1],
                     }}
                     viewport={{ once: true }}
+                    key={index}
                   >
-                    <TableHead>Year</TableHead>
-                    <TableHead>Title</TableHead>
-                    <TableHead className="hide-on-mobile">Made while at</TableHead>
-                    <TableHead className="hide-on-mobile">Build with</TableHead>
-                    <TableHead>Link</TableHead>
+                    <TableData className="year">{new Date(date).getFullYear()}</TableData>
+                    <TableData className="title">{title}</TableData>
+                    <TableData className="made-at hide-on-mobile">
+                      {company ? <span>{company}</span> : <span>--</span>}
+                    </TableData>
+                    <TableData className="build-with hide-on-mobile">
+                      {tech.length &&
+                        tech.map((item, i) => (
+                          <span key={i}>
+                            {" "}
+                            {item} {i !== tech.length - 1 && <span>&middot;</span>}
+                          </span>
+                        ))}
+                    </TableData>
+                    <TableData className="link">
+                      {external && (
+                        <a href={external} aria-label="External link" target="_blank" rel="noreferrer">
+                          <Icon name="External" />
+                        </a>
+                      )}
+                      {github && (
+                        <a href={github} aria-label="GitHub link" target="_blank" rel="noreferrer">
+                          <Icon name="GitHub" />
+                        </a>
+                      )}
+                      {ios && (
+                        <a href={ios} aria-label="GitHub link" target="_blank" rel="noreferrer">
+                          <Icon name="AppStore" />
+                        </a>
+                      )}
+                      {playstore && (
+                        <a
+                          href={playstore}
+                          aria-label="Playstore link"
+                          target="_blank"
+                          rel="noreferrer"
+                          className="playstore-icon"
+                        >
+                          <Icon name="PlayStore" />
+                        </a>
+                      )}
+                    </TableData>
                   </motion.tr>
-                </TableHeader>
-                <TableBody>
-                  {data.map((project, index) => {
-                    const { date, title, company, tech, external, github, ios, playstore } = project.frontmatter;
-
-                    return (
-                      <motion.tr
-                        whileInView={{ opacity: [0, 1], y: [40, 0] }}
-                        transition={{
-                          delay: 0.5,
-                          duration: 0.5,
-                          ease: [0.645, 0.045, 0.355, 1],
-                        }}
-                        viewport={{ once: true }}
-                        key={index}
-                      >
-                        <TableData className="year">{new Date(date).getFullYear()}</TableData>
-                        <TableData className="title">{title}</TableData>
-                        <TableData className="made-at hide-on-mobile">
-                          {company ? <span>{company}</span> : <span>--</span>}
-                        </TableData>
-                        <TableData className="build-with hide-on-mobile">
-                          {tech.length &&
-                            tech.map((item, i) => (
-                              <span key={i}>
-                                {" "}
-                                {item} {i !== tech.length - 1 && <span>&middot;</span>}
-                              </span>
-                            ))}
-                        </TableData>
-                        <TableData className="link">
-                          {external && (
-                            <a href={external} aria-label="External link" target="_blank" rel="noreferrer">
-                              <Icon name="External" />
-                            </a>
-                          )}
-                          {github && (
-                            <a href={github} aria-label="GitHub link" target="_blank" rel="noreferrer">
-                              <Icon name="GitHub" />
-                            </a>
-                          )}
-                          {ios && (
-                            <a href={ios} aria-label="GitHub link" target="_blank" rel="noreferrer">
-                              <Icon name="AppStore" />
-                            </a>
-                          )}
-                          {playstore && (
-                            <a
-                              href={playstore}
-                              aria-label="Playstore link"
-                              target="_blank"
-                              rel="noreferrer"
-                              className="playstore-icon"
-                            >
-                              <Icon name="PlayStore" />
-                            </a>
-                          )}
-                        </TableData>
-                      </motion.tr>
-                    );
-                  })}
-                </TableBody>
-              </Table>
-            </motion.div>
-          )}
-        </ArchiveSection>
-      )}
+                );
+              })}
+            </TableBody>
+          </Table>
+        </motion.div>
+      </ArchiveSection>
     </Wrapper>
   );
 };
