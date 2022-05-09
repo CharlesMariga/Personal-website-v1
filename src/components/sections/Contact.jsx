d7538169dcd82a1122d74240eac7c3c6d6c8cc81
charlesmariga <charlesmariga37@gmail.com>
2022-05-09 09:50:07 +0300

    Added animation to the contact section

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -1,26 +1,57 @@
 import React from "react";
 import styled from "styled-components";
+import { motion } from "framer-motion";
 
 import { SectionWrap } from "../layouts";
+import { usePreferredReducedMotion } from "../../hooks";
 
 const Contact = () => {
+  const prefersReducedMotion = usePreferredReducedMotion();
+
   return (
     <SectionWrap idName="contact">
-      <ContactSection id="contact">
-        <Card>
-          <Number>04.</Number>
-          <TextBox>
-            <Heading>Get in touch</Heading>
-            <Text>
-              Whether you want to ask me a question, or you just want to buy me a coffee ☕😄, don't hesitate to get in
-              touch. My inbox is always open and I will try as much as possible to get back to you.
-            </Text>
-            <Button className="btn btn--full" href="mailTo:charlesmariga37@gmail.com">
-              Contact Me
-            </Button>
-          </TextBox>
-        </Card>
-      </ContactSection>
+      {prefersReducedMotion ? (
+        <ContactSection id="contact">
+          <Card>
+            <Number>04.</Number>
+            <TextBox>
+              <Heading>Get in touch</Heading>
+              <Text>
+                Whether you want to ask me a question, or you just want to buy me a coffee ☕😄, don't hesitate to get
+                in touch. My inbox is always open and I will try as much as possible to get back to you.
+              </Text>
+              <Button className="btn btn--full" href="mailTo:charlesmariga37@gmail.com">
+                Contact Me
+              </Button>
+            </TextBox>
+          </Card>
+        </ContactSection>
+      ) : (
+        <motion.div
+          whileInView={{ opacity: [0, 1], y: [40, 0] }}
+          transition={{
+            delay: 0.5,
+            duration: 0.5,
+            ease: [0.645, 0.045, 0.355, 1],
+          }}
+        >
+          <ContactSection id="contact">
+            <Card>
+              <Number>04.</Number>
+              <TextBox>
+                <Heading>Get in touch</Heading>
+                <Text>
+                  Whether you want to ask me a question, or you just want to buy me a coffee ☕😄, don't hesitate to get
+                  in touch. My inbox is always open and I will try as much as possible to get back to you.
+                </Text>
+                <Button className="btn btn--full" href="mailTo:charlesmariga37@gmail.com">
+                  Contact Me
+                </Button>
+              </TextBox>
+            </Card>
+          </ContactSection>
+        </motion.div>
+      )}
     </SectionWrap>
   );
 };
