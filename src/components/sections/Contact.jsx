88f2b73a82950628c848ad51541006e7aca3c071
charlesmariga <charlesmariga37@gmail.com>
2022-05-09 16:43:38 +0300

    Updated animation in the contact section

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -34,6 +34,7 @@ const Contact = () => {
             duration: 0.5,
             ease: [0.645, 0.045, 0.355, 1],
           }}
+          viewport={{ once: true }}
         >
           <ContactSection id="contact">
             <Card>
